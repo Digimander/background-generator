ea293d0c248606b6b77243da41f9b7c2bb3c32f9
Marek Kováč <makor801@gmail.com>
2022-02-24 10:09:26 +0100

    Initial commit of project files

---
+++ gradientGenerator.js
@@ -0,0 +1,16 @@
+const css = document.querySelector('h3');
+let color1 = document.querySelector('.color1')
+let color2 = document.querySelector('.color2')
+const body = document.getElementById('gradient')
+
+function setGradientColor(){
+    body.style.background =  "linear-gradient(to right,"
+    + color1.value
+    +', '
+    + color2.value
+    +')';
+    css.textContent = body.style.background;
+}
+
+color1.addEventListener("input",setGradientColor)
+color2.addEventListener("input",setGradientColor)
