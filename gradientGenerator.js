fae8848d7fe76b739a472b5c7d5f07bbad9f9154
digimander <makor801@gmail.com>
2022-11-28 22:27:00 +0100

    Setup colors on load and created random colors function

--- gradientGenerator.js
+++ gradientGenerator.js
@@ -1,7 +1,10 @@
+window.addEventListener("load",setGradientColor);
+
 const css = document.querySelector('h3');
 let color1 = document.querySelector('.color1')
 let color2 = document.querySelector('.color2')
 const body = document.getElementById('gradient')
+const button = document.getElementById('random');
 
 function setGradientColor(){
     body.style.background =  "linear-gradient(to right,"
@@ -12,5 +15,16 @@ function setGradientColor(){
     css.textContent = body.style.background;
 }
 
+function random_color(){
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function generateColors(){
+    color1.value = `#${random_color()}`;
+    color2.value = `#${random_color()}`;
+    setGradientColor();
+}
+
 color1.addEventListener("input",setGradientColor)
 color2.addEventListener("input",setGradientColor)
+button.addEventListener("click",generateColors);
